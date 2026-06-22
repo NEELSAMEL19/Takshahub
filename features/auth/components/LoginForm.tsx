968ab620d9632aaea78b45cb85e002957bd27a20
NEELSAMELL369 <neelsamel369@gmail.com>
2026-06-22 10:29:30 +0530

    update ui of components and page

--- features/auth/components/LoginForm.tsx
+++ features/auth/components/LoginForm.tsx
@@ -145,7 +145,7 @@ export function LoginForm() {
   };
 
   return (
-    <div className="flex flex-col gap-4 mx-3 w-full max-w-64 max-h-1/5">
+    <div className="w-full max-w-md mx-auto p-6 sm:p-8 flex flex-col gap-5">
       <span className="theme-text text-5xl text-theme-text">Takshahub</span>
       <span className="text-2xl">Sign in to your account</span>
       <form
