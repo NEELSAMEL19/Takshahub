89e148feed1d938e5dfeb9b3d027074250700544
NEELSAMELL369 <neelsamel369@gmail.com>
2026-07-10 21:00:32 +0530

    update academic module features

---
+++ features/academic/Tabs/TimeTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimeTable = () => {
+  return (
+    <div className="flex items-center justify-center h-50 w-full">
+      <p className="text-5xl font-bold text-gray-500">Coming soon...</p>
+    </div>
+  )
+}
+
+export default TimeTable
